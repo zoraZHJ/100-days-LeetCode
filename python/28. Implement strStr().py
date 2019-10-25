885627aac1ef253aac9c3f6fab5483d637714fcc
jaina <zora_zhj@163.com>
2019-10-25 23:09:26 +0800

    strStr, count and say

--- python/28. Implement strStr().py
+++ python/28. Implement strStr().py
@@ -1,3 +1,10 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        
+        len_stack = len(haystack)
+        len_needle = len(needle)
+        if len_needle == 0:
+            return 0
+        for stack_idx in range(len_stack - len_needle + 1):
+            if haystack[stack_idx : stack_idx + len_needle] == needle:
+                return stack_idx
+        return -1
--- python/38. Count and Say.py
+++ python/38. Count and Say.py
@@ -1,3 +1,22 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        
+        if(n==1):
+            return '1'
+        str_pre = self.countAndSay(n-1)
+        len_pre = len(str_pre)
+
+        print_str = ""
+        cur_num = str_pre[0]
+        i = 1
+        count_num = 1
+        while(i<len_pre):
+            if(str_pre[i] == cur_num):
+                count_num += 1
+            else:
+                print_str = print_str + str(count_num) + cur_num
+                cur_num = str_pre[i]
+                count_num = 1
+            i+=1
+        print_str = print_str + str(count_num) + cur_num
+        return print_str
+            
